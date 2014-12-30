b5dc249c37877d3493258e9bbccccd5485508198
Rasheed Abdul-Aziz and Sai To Yeung <pair+squeedee+syeung@pivotal.io>
2014-12-30 11:18:00 -0500

    Restore ctor so that other tests don't fail

--- go_di/go_di_test.go
+++ go_di/go_di_test.go
@@ -11,21 +11,23 @@ import (
 
 var _ = Describe("GoDi", func() {
 
-	Describe("Integration test", func() {
-		It("has the aceness property", func() {
-			Expect(GoDi()).To(Equal("This widget has the property: aceness\n"))
-		})
-	})
-
 	Describe("Unit test", func() {
-		var fakeRunner fakes.FakeRunner
+		var (
+			fakeRunner fakes.FakeRunner
+			originalNewWidget func(property string) widget.Runner
+		)
 
 		BeforeEach(func() {
 			fakeRunner.RunReturns("fake run")
 
+			originalNewWidget = widget.NewWidget
 			widget.NewWidget = func(property string) widget.Runner {
 				return &fakeRunner
 			}
+		})
+
+		AfterEach(func() {
+			widget.NewWidget = originalNewWidget
 		})
 
 		It("calls run", func() {
@@ -33,6 +35,12 @@ var _ = Describe("GoDi", func() {
 
 			Expect(output).To(Equal("fake run"))
 			Expect(fakeRunner.RunCallCount()).To(Equal(1))
+		})
+	})
+
+	Describe("Integration test", func() {
+		It("has the aceness property", func() {
+			Expect(GoDi()).To(Equal("This widget has the property: aceness\n"))
 		})
 	})
 
